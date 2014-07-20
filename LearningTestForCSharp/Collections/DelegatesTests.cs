9cf67d14bad86c7fe54b89c7a07c82eb8628a6e5
Igor Popov <igor@igorpopov.io>
2014-07-20 20:14:15 +0300

    Add tests for delegates
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ LearningTestForCSharp/Collections/DelegatesTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace LearningTestForCSharp.Collections
+{
+    [TestFixture]
+    public class DelegatesTests
+    {
+        [Test]
+        public void CollectionFilteringWithPredicates()
+        {
+            var numbers = Enumerable.Range(1, 5).ToList();
+            Predicate<int> isEven = number => number % 2 == 0;
+            Assert.That(numbers.FindAll(isEven),
+                Is.EquivalentTo(new[] { 2, 4 }));
+        }
+
+        [Test]
+        public void CollectionFilteringWithLinq()
+        {
+            var numbers = new[] { 1, 2, 3 };
+            Func<int, bool> lessThanTwo = number => number < 2;
+            Assert.That(numbers.Where(lessThanTwo),
+                Is.EquivalentTo(new[] { 1 }));
+        }
+    }
+}
