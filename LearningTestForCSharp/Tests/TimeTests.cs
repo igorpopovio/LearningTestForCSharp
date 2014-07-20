91f541567c820a84120db88c9eaf6e06a2433b44
Igor Popov <igor@igorpopov.io>
2014-07-20 20:59:27 +0300

    Add some time tests
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ LearningTestForCSharp/Tests/TimeTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LearningTestForCSharp.Tests
+{
+    [TestFixture]
+    public class TimeTests
+    {
+        [Test]
+        public void LeapYears()
+        {
+            Assert.That(DateTime.IsLeapYear(2000));
+            Assert.That(DateTime.IsLeapYear(2001), Is.False);
+        }
+    }
+}
