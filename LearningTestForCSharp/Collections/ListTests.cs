0c758cce1ba9e37445e53735271db67076852186
Igor Popov <igor@igorpopov.io>
2014-07-20 12:25:55 +0300

    Learn lists - using the NUnit constraint model assertions
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ LearningTestForCSharp/Collections/ListTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace LearningTestForCSharp.Collections
+{
+    [TestFixture]
+    public class ListTests
+    {
+        [Test]
+        public void ListInitialiazer()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            Assert.That(list, Is.EquivalentTo(new[] { 1, 2, 3 }));
+        }
+
+        [Test]
+        public void ListCounts()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            Assert.That(list, Has.Count.EqualTo(3));
+            Assert.That(list.Count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void ListContains()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            Assert.That(list.Contains(1), Is.True);
+            Assert.That(list, Has.Member(1));
+
+            Assert.That(list.Contains(5), Is.False);
+            Assert.That(list, Has.No.Member(5));
+        }
+
+        [Test]
+        public void RangeCreation()
+        {
+            var list = Enumerable.Range(-3, 3).ToList();
+            var negativeIntegers = new[] { -3, -2, -1 };
+            Assert.That(list, Is.EquivalentTo(expected: negativeIntegers));
+        }
+
+        [Test]
+        public void ConvertAllFromList()
+        {
+            var negativeNumbers = Enumerable.Range(-3, 3).ToList();
+            var positiveNumbers = negativeNumbers.ConvertAll(Math.Abs);
+            Assert.That(positiveNumbers, Is.All.Positive);
+            Assert.That(positiveNumbers, Is.EquivalentTo(new[] { 1, 2, 3 }));
+        }
+    }
+}
